35d2e3ab77d023278d33e2e41cf2571d99f9acb6
jmartinezgr <josemargri3@gmail.com>
2023-09-18 17:56:42 -0500

    Corregido encapsulamiento de atributos a 'protected' en la clase PositionNode En este commit, se ha mejorado el encapsulamiento de los atributos '_fila' y '_columna' en la clase PositionNode, marcándolos como 'protected' mediante la convención del guion bajo. Se han agregado métodos getters y setters para estos atributos para permitir un acceso controlado y seguro a los mismos.

--- node.py
+++ node.py
@@ -9,10 +9,50 @@ class PositionNode:
         :type columna: int
         """
         # Almacena la fila en el grid.
-        self.fila = fila
+        self._fila = fila
 
         # Almacena la columna en el grid.
-        self.columna = columna
+        self._columna = columna
 
         # El siguiente nodo en la cola (inicialmente se establece como None).
-        self.nextNode = None
+        self._nextNode = None
+
+    @property
+    def fila(self):
+        """
+        Obtiene el valor de la fila en el grid.
+
+        :return: El valor de la fila.
+        :rtype: int
+        """
+        return self._fila
+
+    @fila.setter
+    def fila(self, valor):
+        """
+        Establece el valor de la fila en el grid.
+
+        :param valor: El nuevo valor de la fila.
+        :type valor: int
+        """
+        self._fila = valor
+
+    @property
+    def columna(self):
+        """
+        Obtiene el valor de la columna en el grid.
+
+        :return: El valor de la columna.
+        :rtype: int
+        """
+        return self._columna
+
+    @columna.setter
+    def columna(self, valor):
+        """
+        Establece el valor de la columna en el grid.
+
+        :param valor: El nuevo valor de la columna.
+        :type valor: int
+        """
+        self._columna = valor
